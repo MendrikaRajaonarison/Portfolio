8cc73e475b25991b8de1391ca3bc71a08b26ad6a
Mendrika <rajaonarisonmendrikanyaina@gmail.com>
2025-06-21 22:07:04 +0300

    1ere version navbar done, debut connexion db postgreSQL

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,9 +4,12 @@ import { ReactNode } from "react";
 export default function Layout({ children } : { children: ReactNode }) {
     return (
         <div>
-            <nav>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light px-4">
                 <Link className="navbar-brand" href="/">Mon Portfolio</Link>
-                <div className="collapse navbar-collapse">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav ms-auto">
                         <li className="nav-item"><Link className="nav-link" href="/about">A propos</Link></li>
                         <li className="nav-item"><Link className="nav-link" href="/projects">Projets</Link></li>
