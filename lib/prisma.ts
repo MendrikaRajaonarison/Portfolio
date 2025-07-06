8e73de8c565042245e7adb49686afa3fcb1b53a1
Mendrika <rajaonarisonmendrikanyaina@gmail.com>
2025-07-06 22:07:15 +0300

    Connexion à la base de donnée et chargement des projets

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-    var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient};
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+export const prisma = globalForPrisma.prisma || new PrismaClient({ log: ['query']});
 
-export default prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
